0c978461d0c0880ab1d3e6fd5f339d741306989b
LA VAN BAO <lavanbao113@gmail.com>
2024-05-08 07:45:08 +0700

    first commit

---
+++ CHECK HOTMAIL LINKED FACEBOOK/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace CHECK_HOTMAIL_LINKED_FACEBOOK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnImPort = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSaveListTrue = new System.Windows.Forms.Button();
+            this.btnStopp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(14, 9);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(51, 16);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Resutl";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(475, 76);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(127, 41);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnImPort
+            // 
+            this.btnImPort.Location = new System.Drawing.Point(475, 29);
+            this.btnImPort.Name = "btnImPort";
+            this.btnImPort.Size = new System.Drawing.Size(127, 41);
+            this.btnImPort.TabIndex = 3;
+            this.btnImPort.Text = "ImPort";
+            this.btnImPort.UseVisualStyleBackColor = true;
+            this.btnImPort.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(15, 29);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(453, 380);
+            this.txtResult.TabIndex = 4;
+            this.txtResult.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.progressBar1.Location = new System.Drawing.Point(17, 415);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(451, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(475, 123);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(127, 41);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Exit";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnSaveListTrue
+            // 
+            this.btnSaveListTrue.Location = new System.Drawing.Point(476, 171);
+            this.btnSaveListTrue.Name = "btnSaveListTrue";
+            this.btnSaveListTrue.Size = new System.Drawing.Size(126, 51);
+            this.btnSaveListTrue.TabIndex = 7;
+            this.btnSaveListTrue.Text = "Save";
+            this.btnSaveListTrue.UseVisualStyleBackColor = true;
+            this.btnSaveListTrue.Click += new System.EventHandler(this.btnSaveListTrue_Click);
+            // 
+            // btnStopp
+            // 
+            this.btnStopp.Location = new System.Drawing.Point(476, 228);
+            this.btnStopp.Name = "btnStopp";
+            this.btnStopp.Size = new System.Drawing.Size(126, 46);
+            this.btnStopp.TabIndex = 8;
+            this.btnStopp.Text = "Stop";
+            this.btnStopp.UseVisualStyleBackColor = true;
+            this.btnStopp.Click += new System.EventHandler(this.btnStopp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 450);
+            this.Controls.Add(this.btnStopp);
+            this.Controls.Add(this.btnSaveListTrue);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnImPort);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lbl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Check Mail Linked Facebook";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnImPort;
+        private System.Windows.Forms.RichTextBox txtResult;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSaveListTrue;
+        private System.Windows.Forms.Button btnStopp;
+    }
+}
+
---
+++ CHECK HOTMAIL LINKED FACEBOOK/Form1.cs
@@ -0,0 +1,244 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace CHECK_HOTMAIL_LINKED_FACEBOOK
+{
+    public partial class Form1 : Form
+    {
+        private List<string> emails = new List<string>();
+        private bool isChecking = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+
+            if (!isChecking)
+            {
+                isChecking = true;
+                isStopping = false; // Đảm bảo rằng biến isStopping được thiết lập lại khi bắt đầu một quá trình kiểm tra mới
+                var chromeDriverService = ChromeDriverService.CreateDefaultService();
+                chromeDriverService.HideCommandPromptWindow = true;
+
+                // Khởi tạo trình duyệt Chrome
+                ChromeOptions options = new ChromeOptions();
+                options.AddArgument("--window-size=1,1");
+                IWebDriver driver = new ChromeDriver(chromeDriverService, options);
+
+                // Mở trang đăng nhập Facebook
+                driver.Navigate().GoToUrl("https://mbasic.facebook.com/login/identify/?ctx=recover&c=https%3A%2F%2Fmbasic.facebook.com%2F&multiple_results=0&ars=facebook_login&from_login_screen=0&lwv=100&_rdr");
+
+                // Tạo một Thread mới để thực hiện kiểm tra email
+                Thread checkThread = new Thread(() =>
+                {
+                    int totalCount = emails.Count;
+                    int currentCount = 0;
+
+                    foreach (string email in emails)
+                    {
+                        // Kiểm tra biến isStopping để xem liệu quá trình kiểm tra có nên dừng lại không
+                        if (isStopping)
+                        {
+                            // Đóng trình duyệt và kết thúc tiến trình
+                            driver.Quit();
+                            isChecking = false;
+                            return;
+                        }
+
+                        // Tính toán tiến trình
+                        currentCount++;
+                        int progress = (int)(((double)currentCount / totalCount) * 100);
+
+                        // Cập nhật ProgressBar thông qua Invoke
+                        UpdateProgressBar(progress);
+
+                        // Các thao tác kiểm tra email ở đây...
+                        bool isTrue = IsEmailFound(driver, email);
+
+                        // Hiển thị kết quả
+                        string resultText = $"{email} --> {(isTrue ? "True" : "False")}";
+                        AddResultToRichTextBox(resultText, isTrue ? Color.Green : Color.Red);
+                    }
+
+                    // Đóng trình duyệt
+                    driver.Quit();
+
+                    // Đánh dấu đã kết thúc kiểm tra
+                    isChecking = false;
+                });
+
+                // Khởi động Thread
+                checkThread.Start();
+            }
+            else
+            {
+                MessageBox.Show("Đang kiểm tra, vui lòng đợi...");
+            }
+        }
+
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            // Mở hộp thoại chọn tệp
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Đọc nội dung của tệp đã chọn và thêm vào danh sách emails
+                string filePath = openFileDialog.FileName;
+                string[] lines = File.ReadAllLines(filePath);
+                emails.AddRange(lines);
+            }
+        }
+
+        private string ExtractEmail(string input)
+        {
+            // Tách chuỗi theo dấu hai chấm
+            string[] parts = input.Split(':');
+
+            // Trả về phần tử đầu tiên là phần email
+            return parts[0];
+        }
+        private List<string> successfulEmails = new List<string>();
+        private bool IsEmailFound(IWebDriver driver, string emailAndPassword)
+        {
+            // Tách phần email từ chuỗi emailAndPassword
+            string email = ExtractEmail(emailAndPassword);
+
+            // Tiếp tục xử lý kiểm tra email như trước
+            try
+            {
+                // Tìm ô nhập email và điền thông tin
+                IWebElement emailInput = driver.FindElement(By.XPath("//*[@id=\"identify_search_text_input\"]"));
+                emailInput.Clear(); // Xóa dữ liệu trước khi điền mới
+                emailInput.SendKeys(email.Trim());
+
+                // Tìm và ấn nút đăng nhập
+                IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"did_submit\"]"));
+                loginButton.Click();
+
+                // Chờ cho trang load và xử lý kết quả
+                System.Threading.Thread.Sleep(3000); // Thời gian chờ để trang load
+
+                // Kiểm tra nếu có thông báo email không khớp hoặc không tìm thấy tài khoản
+                driver.FindElement(By.XPath("//div[contains(text(),'doesn’t match any account')]"));
+                return false;
+            }
+            catch (NoSuchElementException)
+            {
+                try
+                {
+                    // Kiểm tra nếu có thông báo mật khẩu không đúng
+                    driver.FindElement(By.XPath("//div[contains(text(),'Your search did not return any results. Please try again with other information.')]"));
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    // Nếu không có thông báo lỗi nào xuất hiện, email được tìm thấy
+                    // Thêm email thành công vào danh sách
+                    successfulEmails.Add(email);
+
+                    // Điều hướng tới URL cụ thể
+                    driver.Navigate().GoToUrl("https://mbasic.facebook.com/login/identify/?ctx=recover&c=https%3A%2F%2Fmbasic.facebook.com%2F&multiple_results=0&ars=facebook_login&from_login_screen=0&lwv=100&_rdr");
+
+                    // Tiếp tục quá trình kiểm tra
+                    // Thêm các bước kiểm tra tiếp theo nếu cần
+
+                    return true;
+                }
+            }
+        }
+
+        // Phương thức để lưu danh sách các email kiểm tra thành công vào tệp tin văn bản
+        private bool SaveSuccessfulEmailsToFile(string filePath)
+        {
+            try
+            {
+                // Ghi danh sách email thành công vào tệp tin
+                File.WriteAllLines(filePath, successfulEmails);
+                return true; // Trả về true nếu lưu thành công
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Lỗi khi lưu file: {ex.Message}");
+                return false; // Trả về false nếu có lỗi xảy ra
+            }
+        }
+
+
+
+        private void AddResultToRichTextBox(string text, Color color)
+        {
+            try { 
+            if (txtResult.InvokeRequired)
+            {
+                // Nếu không phải từ luồng UI chính, gọi Invoke để thực hiện
+                txtResult.Invoke(new Action(() => AddResultToRichTextBox(text, color)));
+            }
+            else
+            {
+                // Thêm kết quả vào RichTextBox và chỉnh màu sắc
+                txtResult.SelectionStart = txtResult.TextLength;
+                txtResult.SelectionLength = 0;
+                txtResult.SelectionColor = color;
+                txtResult.AppendText(text + Environment.NewLine);
+                txtResult.SelectionStart = txtResult.TextLength;
+                txtResult.SelectionLength = 0;
+                txtResult.SelectionColor = txtResult.ForeColor; // Reset màu sắc
+            }
+            }catch (Exception ex) { }
+
+        }
+        private void UpdateProgressBar(int progress)
+        {
+            if (progressBar1.InvokeRequired)
+            {
+                // Nếu không phải từ luồng UI chính, gọi Invoke để thực hiện
+                progressBar1.Invoke(new Action(() => UpdateProgressBar(progress)));
+            }
+            else
+            {
+                // Cập nhật giá trị của ProgressBar
+                progressBar1.Value = progress;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSaveListTrue_Click(object sender, EventArgs e)
+        {
+            // Đường dẫn tới tệp tin để lưu danh sách email thành công
+            string filePath = "successful_emails.txt";
+
+            // Thực hiện lưu danh sách email thành công vào tệp tin và kiểm tra kết quả
+            if (SaveSuccessfulEmailsToFile(filePath))
+            {
+                MessageBox.Show("Lưu file thành công");
+            }
+            else
+            {
+                MessageBox.Show("Lưu file không thành công");
+            }
+        }
+
+        private bool isStopping = false;
+        private void btnStopp_Click(object sender, EventArgs e)
+        {
+            isStopping = true;
+        }
+    }
+}
